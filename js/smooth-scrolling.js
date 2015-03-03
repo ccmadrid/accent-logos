886b5d849da65538a543f4c93825563026bb14c8
Brendan  Wilson <brendan307@Macintosh-7.local>
2015-03-03 13:57:53 -0500

    picture gallery

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -10,4 +10,117 @@ $('.logo').localScroll({
             
 });
 
+var $go = $('.go');
+var $big = $('.big');
+var $goone = $('.goone');
+var $gotwo = $('.gotwo');
+var $gothree = $('.gothree');
+var $gofour = $('.gofour');
+var $gofive = $('.gofive');
+var $gosix = $('.gosix');
+var $goseven = $('.goseven');
+var $goeight = $('.goeight');
+var $gonine = $('.gonine');
+var $goten = $('.goten');
+var $goeleven = $('.goeleven');
+var $gotwelve = $('.gotwelve');
+var $gothirteen = $('.gothirteen');
+var $gofourteen = $('.gofourteen');
+
+
+$go.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $go.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$goone.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $goone.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gotwo.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gotwo.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gothree.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gothree.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gofour.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gofour.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gofive.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gofive.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gosix.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gosix.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$goseven.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $goseven.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$goeight.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $goeight.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gonine.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gonine.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$goten.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $goten.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$goeleven.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $goeleven.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gotwelve.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gotwelve.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gothirteen.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gothirteen.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+$gofourteen.on('click', function (e) {
+    e.preventDefault();
+    var bigPic = $gofourteen.attr('href');
+    $big.css('background-image', 'url(' + bigPic + ')');
+});
+
+
+
+
+
+
 
